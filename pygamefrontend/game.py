76d81c40999726fec8a6b2518ee3a9552ab87324
orneo1212 <orneo1212@gmail.com>
2011-10-10 05:03:58 +0200

    Refactoring

--- pygamefrontend/game.py
+++ pygamefrontend/game.py
@@ -18,7 +18,7 @@ class Game:
         #speeds
         self.movespeed=0.25
         self.mineticks=10 # number of ticks to mine
-        self.minetick=0
+        self.minetickcounter=0
         #times
         self.eventstime=time.time()
         self.eventdelay=0.025
@@ -36,7 +36,7 @@ class Game:
             self.starttime=time.time()
             self.mapviewer.updatedattime()
         #Grow
-        if self.minetick%100==0:
+        if self.minetickcounter%1000==0:
             secp=self.mapo.convertposition(self.player.getposition())
             sector=self.mapo.getsector(secp[0])
             engine.map.randomgrow(sector)
@@ -45,54 +45,55 @@ class Game:
 
     def events(self):
         keys=pygame.key.get_pressed()
+        mousekeys=pygame.mouse.get_pressed()
+        #get event from queue
         event=pygame.event.poll()
+
         if event.type==pygame.QUIT:self.onexit()
+
         #events tick
-        if time.time()>self.eventstime+self.eventdelay:
-            self.eventstime=time.time()
-            self.minetick+=1
+        if time.time()<self.eventstime+self.eventdelay:return
 
-            plpos=self.player.getposition()
-            mx, my=pygame.mouse.get_pos()
-            mtx, mty=self.mapviewer.getglobalfromscreen(plpos, (mx, my))
+        #ALL LINES BELOW WILL BE CALLED ONE PER EVENT TICK
 
-            #keys
+        self.eventstime=time.time()
+        self.minetickcounter+=1
+        #keys
 
-            #time change
-            if keys[pygame.K_F3]:
-                self.mapviewer.daytime+=5
-            if keys[pygame.K_F4]:
-                self.mapviewer.daytime-=5
-            #directions
-            if keys[pygame.K_d]:
-                self.player.move("e", self.movespeed)
-            if keys[pygame.K_a]:
-                self.player.move("w", self.movespeed)
-            if keys[pygame.K_w]:
-                self.player.move("n", self.movespeed)
-            if keys[pygame.K_s]:
-                self.player.move("s", self.movespeed)
-            if keys[pygame.K_SPACE]:
-                print "Position:", mtx, mty
-                print "FPS:", self.gametimer.get_fps()
-                print "Inventory:"
-                for slot in self.player.inventory.slots:
-                    if slot:print "Item ID=%s Count=%s" % (slot[0], slot[1])
-            #Mouse events
-            mousekeys=pygame.mouse.get_pressed()
-            #mine block
-            if mousekeys[0]==1:
-                self.mineblock((mtx,mty))
-            #get block under cursor
-            if mousekeys[1]==1:
-                block=self.mapo.getblock((mtx, mty))
-                if block:self.currenttile=block.id
-                else:self.currenttile=None
-            #put block
-            if mousekeys[2]==1:
-                self.putblock((mtx,mty))
-            #Send events to pages
-            self.ingamescreen.events(event)
+        #get mouse pos and calculate block position
+        plpos=self.player.getposition()
+        mx, my=pygame.mouse.get_pos()
+        mtx, mty=self.mapviewer.getglobalfromscreen(plpos, (mx, my))
+
+        #time change
+        if keys[pygame.K_F3]:
+            self.mapviewer.daytime+=5
+        if keys[pygame.K_F4]:
+            self.mapviewer.daytime-=5
+        #directions
+        if keys[pygame.K_d]:
+            self.player.move("e", self.movespeed)
+        if keys[pygame.K_a]:
+            self.player.move("w", self.movespeed)
+        if keys[pygame.K_w]:
+            self.player.move("n", self.movespeed)
+        if keys[pygame.K_s]:
+            self.player.move("s", self.movespeed)
+        if keys[pygame.K_SPACE]:
+            print "FPS:", self.gametimer.get_fps()
+        #mine block
+        if mousekeys[0]==1:
+            self.mineblock((mtx,mty))
+        #get block under cursor
+        if mousekeys[1]==1:
+            block=self.mapo.getblock((mtx, mty))
+            if block:self.currenttile=block.id
+            else:self.currenttile=None
+        #put block
+        if mousekeys[2]==1:
+            self.putblock((mtx,mty))
+        #Send events to pages
+        self.ingamescreen.events(event)
 
     def actioninrange(self,actionpos):
         plpos=self.player.getposition()
@@ -104,14 +105,14 @@ class Game:
     def putblock(self,mousepos):
         #avoid putblock on player position
         if mousepos!=self.player.getposition():
-            if self.actioninrange(mousepos):
-                if self.minetick%5==0:
+            if self.minetickcounter%5==0:
+                if self.actioninrange(mousepos):
                     err=self.player.putblock(mousepos, self.currenttile)
 
     def mineblock(self,mousepos):
         plpos=self.player.getposition()
         if self.actioninrange(mousepos):
-            if self.minetick%self.mineticks==0:
+            if self.minetickcounter%self.mineticks==0:
                 err=self.player.mineblock(mousepos)
 
     def redraw(self):
