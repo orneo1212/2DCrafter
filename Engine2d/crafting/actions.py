11a83aad322b84a87dada2771ad7fc98c3411bb2
orneo1212 <orneo1212@gmail.com>
2011-10-12 13:55:28 +0200

    Fixed crafting bug when nventory is full

--- Engine2d/crafting/actions.py
+++ Engine2d/crafting/actions.py
@@ -6,9 +6,17 @@ def craft(player,recipename):
         player.inventory.getitems()):
         #can craft
         recipe=engine.crafting.getrecipe(recipename)
-        for item in recipe["req"]:
-            player.inventory.removeitem(item)
+        #simulate add result
         for cc in range(recipe["count"]):
-            player.inventory.additem(recipe["result"])
+            err=player.inventory.additem(recipe["result"],True)
+            if err:break
+        if err:return 1 # cannot craft
+        else:
+            #real add items
+            for cc in range(recipe["count"]):
+                err=player.inventory.additem(recipe["result"])
+            #remove items
+            for item in recipe["req"]:
+                player.inventory.removeitem(item)
         return 0 # Done
     else:return 1 # cannot craft
