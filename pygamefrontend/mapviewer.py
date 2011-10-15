39bfdd630125a21762eff6bb7210108f83f15173
orneo1212 <orneo1212@gmail.com>
2011-10-15 11:29:54 +0200

    MapViewer refactor

--- pygamefrontend/mapviewer.py
+++ pygamefrontend/mapviewer.py
@@ -9,8 +9,11 @@ class MapViever:
     """Map Viewer for pygame frontend"""
     def __init__(self):
         SW,SH=pygamefrontend.SW,pygamefrontend.SH
+        #View Width and height (in tiles)
         self.viewW=25
         self.viewH=18
+        self.center=(self.viewW/2,self.viewH/2) # centered position
+        #tilesize
         self.tilesize=pygamefrontend.TILESIZE
         #light
         self.lightsurface=pygame.Surface((SW,SH),pygame.SRCALPHA)
@@ -19,6 +22,16 @@ class MapViever:
         self.playerimg=pygamefrontend.imgloader.loadimage("player")
         self.backimg=pygamefrontend.imgloader.loadimage("backimg")
         self.blockimages=pygamefrontend.imgloader.loadimages()
+        #player position
+        self.playerx=(self.viewW/2)*self.tilesize
+        self.playery=(self.viewH/2)*self.tilesize
+
+    def calculate_drawpos(self,tiledposition,centered=(0,0)):
+        """Calculate where object at tiledpositions (centered at center)
+        should be drawn on the screen"""
+        locx=self.center[0]+centered[0]-tiledposition[0]
+        locy=self.center[1]+centered[1]-tiledposition[1]
+        return (locx*self.tilesize,locy*self.tilesize)
 
     def render(self, surface, center, mapobject):
         """Render map on the surface. Map will be centered on center position (global)."""
@@ -29,29 +42,22 @@ class MapViever:
         lightlevel=engine.environment.DAYTIME.getlightlevel()
         #fill mask layer
         self.lightsurface.fill((0,0,0,lightlevel))
-        hW=self.viewW/2
-        hH=self.viewH/2
         #render tiles
-        for yy in range(cy-hH, cy+hH+1):
-            locy=hH+cy-yy
-            drawposy=locy*tilesize
-            for xx in range(cx-hW, cx+hW+1):
-                locx=hW+cx-xx
-
+        for yy in range(cy-self.center[1], cy+self.center[1]+1):
+            for xx in range(cx-self.center[0], cx+self.center[0]+1):
                 drawblock=True
-
                 #get block and blit it
                 block=mapobject.getblock((xx, yy))
                 #check for air and empty blocks
                 if not block:drawblock=False # skip empty places
                 elif block.id==0:drawblock=False # skip block with id 0
 
-                drawposx=locx*tilesize
+                drawpos=self.calculate_drawpos((xx,yy),(cx,cy))
 
-                drawpos=(drawposx,drawposy)
                 #light position
-                lx,ly=drawposx+self.lightoffset,\
-                    drawposy+self.lightoffset
+                #TODO: move to functions.py
+                lx,ly=drawpos[0]+self.lightoffset,\
+                    drawpos[1]+self.lightoffset
 
                 #draw block only if there is one
                 if drawblock:
@@ -64,13 +70,16 @@ class MapViever:
                 #if not draw background image
                 else:
                     surface.blit(self.backimg, drawpos)
-                #render player image on center position
-                if xx==cx and yy==cy:
-                    surface.blit(self.playerimg, drawpos)
-                    #draw light emited by player
-                    #functions.drawlight(self.lightsurface,(lx,ly),4)
+
+        #draw light emited by player
+        #functions.drawlight(self.lightsurface,(lx,ly),4)
         #blit light mask
         surface.blit(self.lightsurface,(0,0))
+
+    def drawplayer(self,surface):
+        """Draw player centered at screen"""
+        #render player image on center position
+        surface.blit(self.playerimg, (self.playerx, self.playery))
 
     def getglobalfromscreen(self,centerpos,screenpos):
         """get global position (in tiles) from screenpos,
@@ -85,3 +94,4 @@ class MapViever:
     def renderatplayer(self,surface,player,mapobject):
         """Render map centered on given player"""
         self.render(surface, player.getposition(), mapobject)
+        self.drawplayer(surface)
